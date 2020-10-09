adce2298b91ff82ee0b07fd31899b1c386972f24
Daviduf11 <ufberg.david@gmail.com>
2020-10-09 15:32:59 -0400

    completed calculator functionality

--- tip-calc.js
+++ tip-calc.js
@@ -11,13 +11,21 @@ const totalPartySize = readlineSync.questionInt('What is the size of your party?
 const tipPercentage = readlineSync.questionFloat("What percentage would you like to tip? ");
 
 // repeat all those things back to the user
-console.log(typeof totalAmount, typeof totalPartySize, typeof tipPercentage);
+// console.log(`The total amount of your bill is: $${totalAmount.toFixed(2)}`);
+// console.log(`Your total party size is: ${totalPartySize}`);
+// console.log(`The tip percentage being applied is: ${tipPercentage}`);
 
 // tip = usertip * total bill 
+let totalTip = totalAmount * (tipPercentage / 100); 
 
 // total bill = bill + tip
+let billWithTip = totalAmount + totalTip;
 
-
+// get total bill divided by number of people 
+let totalPerPerson = billWithTip / totalPartySize; 
 
 // calculate how much everyone is paying. ( total bill * tip% ) / party size
+console.log(`The total tip is: $${totalTip.toFixed(2)}`);
+console.log(`The total bill is: $${billWithTip.toFixed(2)}`);
+console.log(`The total bill per person is: ${totalPerPerson.toFixed(2)}`);
 
